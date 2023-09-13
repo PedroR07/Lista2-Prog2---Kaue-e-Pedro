b3808b6d5576a65ef19153ea3f6452230bbd2f9e
ParanoidAndroid <136632613+PedroR07@users.noreply.github.com>
2023-09-13 17:53:32 -0300

    Add files via upload

---
+++ Exercicio06.java
@@ -0,0 +1,121 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        int[] distCent = new int [15];
+        int[] qteVeranistas = new int [15];
+        int[] pavimentacao = new int [15];
+        int q = 0, n;
+        while(q == 0){
+            n = Integer.parseInt(JOptionPane.showInputDialog("Selecione a Operacão: \n1- Adicionar ou editar dados. \n2- Consultar dados."));
+            switch (n) {
+                case 1:
+                    addDados(qteVeranistas, distCent, pavimentacao);
+                    break;
+                case 2:
+                    consultInformacao(qteVeranistas, distCent, pavimentacao);
+                    break;
+                case 3:
+                    q = 1;
+                    break;
+            }
+        }
+    }
+    public static void addDados(int[] A, int[] B, int[] C){
+        int q = 0, ID, selector;
+        while(q == 0){
+        ID = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira o ID da praia (de 0 à 15): "));
+        selector = Integer.parseInt(JOptionPane.showInputDialog(null, "Selecione a operação: \n1- Alterar quantidade de veranistas. \n2- Alterar distância. \n3- Alterar estado da pavimentação. \n4- Voltar ao menu principal."));
+        switch (selector){
+            case 1:
+                A[ID] = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira a quantidade de veranistas: "));
+                break;
+            case 2:
+                B[ID] = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira a distância: "));
+                break;
+            
+            case 3:
+                B[ID] = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira o estado da pavimentação: "));
+                break;
+            case 4:
+                q = 1;
+                break;
+        }
+        }
+    }
+    public static void consultInformacao(int[] A, int[] B, int[] C){
+        int n, q = 0;
+        while(q == 0){
+            n = Integer.parseInt(JOptionPane.showInputDialog(null, "Selecione a informação: \n1- Praia mais distante do centro. \n2- Praia mais próxima do centro. \n3- Praia menos frequentada. \n4- Praias que estão a mais de 15km do centro. \n5- Número de veranistas em praias não pavimentadas. \n6- Número de veranistas em praias pavimentadas. \n7- Voltar ao menu principal."));
+            switch (n) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, maiorDist(B));
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, menorDist(B));
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, praiaMenosFreq(A));
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, praiaMaisQuinze(B));
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                break;
+                case 7:
+                    q = 1;
+                    break;
+            }
+        }
+    }
+    public static String maiorDist(int B[]){
+        String n;
+        int maiorD = -999999999, i, iD = 0;
+        for(i = 0; i < 15; i++){
+            if(B[i] > maiorD){
+                maiorD = B[i];
+                iD = i;
+            }
+        }
+        n = ("A praia mais distante do centro é a praia " + iD + ", com " + maiorD + " metros de distância.");
+        return n;
+    }
+    public static String menorDist(int B[]){
+        String n;
+        int menorD = 999999999, i, iD = 0;
+        for(i = 0; i < 15; i++){
+            if(B[i] < menorD && B[i] > 0){
+                menorD = B[i];
+                iD = i;
+            }
+        }
+        n = ("A praia mais próxima do centro é a praia " + iD + ", com " + menorD + " metros de distância.");
+        return n;
+    }
+    public static String praiaMenosFreq(int A[]){
+        String n;
+        int menorD = 999999999, i, iD = 0;
+        for(i = 0; i < 15; i++){
+            if(A[i] < menorD && A[i] > 0){
+                menorD = A[i];
+                iD = i;
+            }
+        }
+        n = ("A praia menos veranistas é a " + iD + ", com " + menorD + " veranistas.");
+        return n;
+    }
+    public static String praiaMaisQuinze(int B[]){
+        String mais15 = "", mais15R = "";
+        for(int i = 0; i < 15; i++){
+            if (B[i] >= 15) {
+                    mais15 += Integer.toString(i) + ", ";
+            }
+        }
+        mais15R = "As praias com mais de 15km do centro são as praias " + mais15 + ".";
+        return mais15R;
+    }
+}
